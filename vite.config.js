c997dab28f586144b4231a69b065c414546841d7
SaranshMishra926 <saranshmishra1529@gmail.com>
2025-12-14 22:01:29 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SCT_WD_1/',
   server: {
     port: 3000,
     open: true
   }
 })
-
